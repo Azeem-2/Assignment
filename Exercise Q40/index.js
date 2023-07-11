43df21e1d45df4d960e39a8ed6133f8253acaaea
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-11 16:54:23 +0500

    .

---
+++ Exercise Q40/index.js
@@ -0,0 +1,9 @@
+// Task 40:Magician
+function showMagicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+var magicians = ['David Copperfield', 'Harry Houdini', 'Penn & Teller'];
+showMagicians(magicians);
---
+++ Exercise Q40/index.ts
@@ -0,0 +1,9 @@
+// Task 40:Magician
+function showMagicians(magicians: string[]) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+let magicians: string[] = ['David Copperfield', 'Harry Houdini', 'Penn & Teller'];
+showMagicians(magicians);
