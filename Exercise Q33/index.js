51101d1b565de19a55c9ca012bb15bbb94e177ca
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-11 16:39:32 +0500

    .

---
+++ Exercise Q33/index.js
@@ -0,0 +1,7 @@
+// Task 33:Pizzas
+var pizzas = ['pepperoni', 'margherita', 'hawaiian'];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log('I really love pizza!');
---
+++ Exercise Q33/index.ts
@@ -0,0 +1,8 @@
+// Task 33:Pizzas
+let pizzas: string[] = ['pepperoni', 'margherita', 'hawaiian'];
+
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log('I really love pizza!');
