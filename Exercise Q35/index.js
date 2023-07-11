5032446b11c30ef2f5be0ed753ad88d6b178dbfc
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-11 16:43:38 +0500

    .

---
+++ Exercise Q35/index.js
@@ -0,0 +1,5 @@
+// Task 35:T-shirt
+function makeShirt(size, message) {
+    console.log("The shirt is size ".concat(size, " and has the message '").concat(message, "' printed on it."));
+}
+makeShirt('Large', 'Hello World!');
---
+++ Exercise Q35/index.ts
@@ -0,0 +1,6 @@
+// Task 35:T-shirt
+function makeShirt(size: string, message: string) {
+    console.log(`The shirt is size ${size} and has the message '${message}' printed on it.`);
+}
+
+makeShirt('Large', 'Hello World!');
