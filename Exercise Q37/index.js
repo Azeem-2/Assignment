c04df4bd4bb67e45ff2221865f77080e60cdeda0
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-11 16:47:56 +0500

    .

---
+++ Exercise Q37/index.js
@@ -0,0 +1,8 @@
+// Task 37:Cities
+function describeCity(city, country) {
+    if (country === void 0) { country = 'Pakistan'; }
+    console.log("".concat(city, " is in ").concat(country, "."));
+}
+describeCity('Karachi');
+describeCity('Lahore');
+describeCity('New York', 'United States');
---
+++ Exercise Q37/index.ts
@@ -0,0 +1,8 @@
+// Task 37:Cities
+function describeCity(city: string, country: string = 'Pakistan') {
+    console.log(`${city} is in ${country}.`);
+}
+
+describeCity('Karachi');
+describeCity('Lahore');
+describeCity('New York', 'United States');
