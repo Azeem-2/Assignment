791bc43ff38749f74ca6a931af267f70948aee91
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-11 16:50:13 +0500

    .

--- Exercise Q38/index.js
+++ Exercise Q38/index.js
@@ -1,8 +1,7 @@
 // task 38:Cities Name
-function cityCountry(city: string, country: string): string {
-    return `${city}, ${country}`;
+function cityCountry(city, country) {
+    return "".concat(city, ", ").concat(country);
 }
-
 console.log(cityCountry('Lahore', 'Pakistan'));
 console.log(cityCountry('New York', 'United States'));
 console.log(cityCountry('London', 'United Kingdom'));
